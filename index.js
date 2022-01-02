a3af1032a320da658ac19b12f0625667ad8a6718
Richard <rcoric03@gmail.com>
2022-01-02 10:26:31 +0000

    Add Test Help Command with Demo Embed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 const CommandHandler = require("./src/CommandHandler");
 
 const dotenv = require("dotenv");
@@ -20,8 +20,34 @@ client.on("messageCreate", (message) => {
   }
 });
 
+// Abstract out the code that will be repeated.
 handler.use("welcome", (message) => {
-  message.channel.send("Welcome to the server!");    
-})
+  message.channel.send("Welcome to the server!");
+});
+
+// help command - creates a help embed pushing the commands from the handler
+handler.use("help", (message) => {
+  const embed = {
+    color: 0xfd79a8,
+    title: "**Help**",
+    description: "This is a list of commands.",
+    fields: [],
+    timestamp: new Date(),
+	  footer: {
+	  	text: 'More Commands coming soon!',
+	  	icon_url: 'https://i.imgur.com/AfFp7pu.png',
+	  },
+  };
+  handler.commands.forEach((command) => {
+    embed.fields.push({
+      name: `***${handler.prefix}${command}***`,
+      value: `${command} command.`,
+    });
+  });
+  
+
+  message.channel.send({ embeds: [embed] });
+});
+
 
 client.login(process.env.TOKEN);
--- src/CommandHandler.js
+++ src/CommandHandler.js
@@ -3,15 +3,19 @@ class CommandHandler{
     constructor(obj) { 
         this.client = obj.client;
         this.prefix = obj.prefix;
+        this.commands = [];
     }
 
     use(name, callback) {
+        this.commands.push(name);
         this.client.on("messageCreate", (message) => {
-            if (message.content.toLocaleLowerCase() == `${this.prefix}${name}`) {
+            let command = message.content.toLocaleLowerCase();
+            if (command === `${this.prefix}${name}`) {
                 callback(message);
             }
         });
     }
+    
 }
 
 module.exports = CommandHandler;
